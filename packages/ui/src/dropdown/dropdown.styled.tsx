73b4b8ce2a39e797971c6167e56ae0bdd5fa2fbe
danielzeman <daniel.zeman@creativedock.cz>
2019-10-07 18:19:57 +0200

    191007 - beginning of household-ui package

---
+++ packages/ui/src/dropdown/dropdown.styled.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from 'styled-components'
+import { HHSimple, HHSimpleProps } from '@householdjs/layout'
+import { DropdownStyledProps } from './dropdown.types'
+import { CLASS_IS_OPEN } from '../constants'
+import { pxToRem, withTransition } from '@householdjs/utils'
+
+
+export const DropdownStyled = styled(({...rest}: HHSimpleProps) => <HHSimple {...rest} />)<DropdownStyledProps>(({maxHeight}: DropdownStyledProps) => ({
+	overflow: "hidden",
+	pointerEvents: "none",
+	maxHeight: 0,
+	[`&.${CLASS_IS_OPEN}`]: {
+		pointerEvents: "auto",
+		maxHeight: pxToRem(maxHeight),
+	},
+	...withTransition("max-height"),
+}))
---
+++ packages/ui/src/dropdown/dropdown.types.ts
@@ -0,0 +1,6 @@
+
+import { HHSimpleProps } from '@householdjs/layout'
+import { DropdownProps, DropdownState } from './dropdown'
+
+export type DropdownStyledProps = Omit<DropdownProps, "checkDOMUpdate" | "isOpen" | "maxHeightDefined"> &
+	DropdownState & HHSimpleProps;
