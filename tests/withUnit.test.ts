24cc110231f4b1678e9ce0665f33b6bf7ef761ab
Daniel Zeman <ledaniel.zeman@gmail.com>
2020-01-11 01:41:41 +0100

    200110 - unit tests path fix

--- tests/withUnit.test.ts
+++ tests/withUnit.test.ts
@@ -1,4 +1,4 @@
 import { toTest } from './tests.utils'
-import { withUnit } from '../packages/utils/src'
+import { withUnit } from '../packages/householdjs-utils/src'
 
 toTest('withUnit - 20', withUnit, [20, 'rem'], '20rem')
