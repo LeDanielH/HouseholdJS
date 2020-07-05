b22f00925eb79d541957bd06a2cc989169c43265
danielzeman <daniel.zeman@nangu.tv>
2020-07-05 11:20:38 +0200

    200705 - temporarily added some utils

--- packages/householdjs-utils/src/index.ts
+++ packages/householdjs-utils/src/index.ts
@@ -9,6 +9,10 @@ export { multiplyStringValue, mvws } from './multiply-string-value'
 export { pxToRem } from './px-to-rem'
 export { withPseudo, withAfter, withBefore } from './with-pseudo'
 export { withTransition } from './with-transition'
+
+// TODO: I need them temporarily, will move them to another package altogether
+export { round, roundDown, roundUp, getNumber, getPositiveNumber, getRoundedNumber, isZeroOrPositiveNumber, isPositiveNumber } from './numbersMod'
+
 export {
 	withUnit,
 	withRem,
