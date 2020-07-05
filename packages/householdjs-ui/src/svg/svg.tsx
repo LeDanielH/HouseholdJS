a9bb787777d047391fba846cb2a33f433e291969
danielzeman <daniel.zeman@nangu.tv>
2020-07-05 12:26:16 +0200

    200705 - svg ported from react-native + flow fixes

--- packages/householdjs-ui/src/svg/svg.tsx
+++ packages/householdjs-ui/src/svg/svg.tsx
@@ -3,26 +3,12 @@
 import React, { ReactNode } from 'react'
 import { calculateSvgSize } from './svg.utils'
 import { SvgProps } from '@householdjs/types'
-import { getNumber } from '@householdjs/utils'
-
-/**
- * HOW TO USE
- * copy and save viewBox width and viewBox height from original html <svg> element
- * replace html <svg> by <SvgNative>
- * pass viewBox values to <SvgNative viewBoxWidth={VIEW_BOX_WIDTH} viewBoxHeight={VIEW_BOX_HEIGHT}>
- * change the rest of html svg elements to capitalCase and all of its attributes to lowerCamelCase
- * import capitalCased elements from 'react-native-svg'
- * check PassedProps
- */
 
 type ViewBoxSize = {
-	viewBoxWidth?: number // you copy it from original svg element - never change this value!! - paths inside svg are calculations based on viewBox
-	viewBoxHeight?: number // you copy it from original svg element - never change this value!! - paths inside svg are calculations based on viewBox
-	viewBoxSize: number // when viewBoxWidth and viewBoxHeight are the same
+	viewBoxSize: number
 }
 
 type ViewBoxValues = {
-	viewBoxSize?: number
 	viewBoxWidth: number
 	viewBoxHeight: number
 }
@@ -31,16 +17,13 @@ type ViewBox = ViewBoxSize | ViewBoxValues
 
 type PassedProps = ViewBox & {
 	children: ReactNode
-	size?: number // if width and height are not the same, it defines the longer side width -> the other side is calculated
-	// if not defined, viewBoxSize or the bigger of viewBoxWidth and viewBoxHeight is the default size of the svg
-	overflowFixScaleRatio?: number // useful for charts => when you define stroke, it can overflow the viewBox, making part of the svg invisible
-	// you can scale down the content inside viewBox a little in order to make the missing part of the stroke visible
-	overflowFixPosition?: number // useful for charts => when you use overflowFixScaleRatio, the paths inside viewBox might need a little adjustment nudge
+	size?: number
+	overflowFixScaleRatio?: number
+	overflowFixPosition?: number
 }
 
 export type Props = PassedProps & Partial<SvgProps>
 
-// eslint-disable-next-line func-style
 export const Svg = ({
 	size,
 	viewBoxWidth,
@@ -53,14 +36,8 @@ export const Svg = ({
 }: Props) => {
 	const vbWidth = viewBoxSize || viewBoxWidth
 	const vbHeight = viewBoxSize || viewBoxHeight
-	const vbHeightNumber = getNumber(vbWidth)
-	const vbWidthNumber = getNumber(vbHeight)
 
-	const { width, height } = calculateSvgSize(
-		vbWidthNumber,
-		vbHeightNumber,
-		size
-	)
+	const { width, height } = calculateSvgSize(vbWidth, vbHeight, size)
 
 	const viewBox = `0 0 ${vbWidth} ${vbHeight}`
 
