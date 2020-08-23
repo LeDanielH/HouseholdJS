dbf19f2fa74bdb0ab0592b72e24914aeedda7004
ledanielz <ledaniel.zeman@gmail.com>
2020-08-23 13:48:40 +0200

    200822 - new getItemsPosition helper

---
+++ packages/householdjs-utils/src/getItemsPosition.ts
@@ -0,0 +1,21 @@
+import { round } from './numbersMod.ts'
+
+export function getItemsPosition(item: HTMLElement) {
+	const box = item.getBoundingClientRect()
+
+	const { body } = document
+	const { documentElement } = document
+
+	const scrollTop =
+		window.pageYOffset || documentElement.scrollTop || body.scrollTop
+	const scrollLeft =
+		window.pageXOffset || documentElement.scrollLeft || body.scrollLeft
+
+	const clientTop = documentElement.clientTop || body.clientTop || 0
+	const clientLeft = documentElement.clientLeft || body.clientLeft || 0
+
+	const top = box.top + scrollTop - clientTop
+	const left = box.left + scrollLeft - clientLeft
+
+	return { x: round(left), y: round(top) }
+}
